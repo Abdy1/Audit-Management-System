3327b482186abbbb2554f255826e62522f931f8f
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2023-10-03 15:54:06 +0300

    dto added

--- src/main/java/com/cbo/audit/persistence/model/User.java
+++ src/main/java/com/cbo/audit/persistence/model/User.java
@@ -1,9 +1,27 @@
 package com.cbo.audit.persistence.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ams_user")
 public class User extends BaseEntity{
+    @Column(name = "username", unique = true, nullable = false, length = 20)
+    private String username;
+    @Column(nullable = false, length = 64)
+    private String password;
+    @Column(name = "active")
+    private Boolean active;
+    @Column(name = "createdAt")
+    private String createdAt;
+    @Column(name = "updatedAt")
+    private String updatedAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employee;
 }

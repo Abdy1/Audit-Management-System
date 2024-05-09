5b2be4a94992db739d053f150a156a55971e9cf5
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2024-05-09 14:36:01 +0300

    team, find by status

--- src/main/java/com/cbo/audit/persistence/repository/AuditTeamRepository.java
+++ src/main/java/com/cbo/audit/persistence/repository/AuditTeamRepository.java
@@ -1,11 +1,13 @@
 package com.cbo.audit.persistence.repository;
 
+import com.cbo.audit.enums.AuditStaffStatus;
 import com.cbo.audit.persistence.model.AuditStaff;
 import com.cbo.audit.persistence.model.AuditTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AuditTeamRepository extends JpaRepository<AuditTeam, Long> {
@@ -15,4 +17,8 @@ public interface AuditTeamRepository extends JpaRepository<AuditTeam, Long> {
 
     @Query("SELECT AT FROM AuditStaff AT WHERE AT.employeeId = :id")
     Optional<AuditTeam> findAuditTeamByEmployeeId(@Param("id") String employeeId);
+
+    @Query(" SELECT AST FROM AuditTeam AST  "
+            + " WHERE AST.status = :status")
+    List<AuditTeam> findAuditTeamByState(@Param("status") AuditStaffStatus status);
 }

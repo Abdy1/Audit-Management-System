3faf12f12eb3196e8b4c4cbe12b5e2afcc80a1c0
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2024-05-10 11:12:50 +0300

    file upload

--- src/main/java/com/cbo/audit/controller/FindingCommentController.java
+++ src/main/java/com/cbo/audit/controller/FindingCommentController.java
@@ -3,12 +3,15 @@ package com.cbo.audit.controller;
 import com.cbo.audit.dto.FindingCommentDTO;
 import com.cbo.audit.dto.ResultWrapper;
 import com.cbo.audit.service.impl.FindingCommentServiceImpl;
+import com.cbo.audit.utils.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 import static com.cbo.audit.constants.URIs.*;
@@ -36,6 +39,20 @@ public class FindingCommentController {
 
         ResultWrapper<FindingCommentDTO> resultWrapper = findingCommentService.updateCommentFinding(findingCommentDTO);
         return new ResponseEntity<>(resultWrapper, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/upload")
+    public String handleFileUpload(@RequestParam("finding/files") MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            String originalFilename = file.getOriginalFilename();
+            // Process the file name (e.g., store it, validate it, etc.)
+            // ...
+            FileUploadUtil.saveFile("sample/", "1" + file.getOriginalFilename(), file);
+
+            return "File uploaded successfully: " + originalFilename;
+        } else {
+            return "No file uploaded.";
+        }
     }
 
 }
